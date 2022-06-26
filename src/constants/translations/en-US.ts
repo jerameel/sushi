719f02c17b756f25269b21b4ce66c045f0eaa301
Jerameel <jerameeldelosreyes@gmail.com>
2022-06-26 21:47:00 +0800

    Add initial language translation english mapping

--- src/constants/translations/en-US.ts
+++ src/constants/translations/en-US.ts
@@ -1,7 +1,50 @@
 import { Translation } from 'types/Translation';
 
 const en_US: Translation = {
-  label: 'English (US)',
+  // DASHBOARD
+  CREDIT: 'Credit',
+  DEBIT: 'Debit',
+  RECENT_TRANSACTIONS: 'Recent Transactions',
+  MY_ACCOUNTS: 'My Accounts',
+
+  // ADD ACCOUNT
+  ADD_ACCOUNT: 'Add Account',
+  ACCOUNT_NAME: 'Account Name',
+  INITIAL_AMOUNT: 'Initial Amount',
+
+  // ACCOUNT DETAILS
+  ACCOUNT_DETAILS: 'Account Details',
+  INITIAL_BALANCE: 'Initial Balance',
+
+  // NEW TRANSACTION
+  NEW_TRANSACTION: 'New Transaction',
+  CREATE_TRANSACTION: 'Create Transaction',
+  CATEGORY: 'Category',
+  SOURCE_ACCOUNT: 'Source Account',
+  DESTINATION_ACCOUNT: 'Destination Account',
+  TRANSFER: 'Transfer',
+  SHORT_DESCRIPTION: 'Short Description',
+  AMOUNT: 'Amount',
+
+  // TRANSACTION DETAILS
+  ACCOUNT: 'Account',
+  DATE_CREATED: 'Date Created',
+  DATE_UPDATED: 'Date Updated',
+
+  // Transactions
+  TRANSACTIONS: 'Transactions',
+  SEARCH: 'Search',
+  DATE_RANGE: 'Date Range',
+  SHOW_ALL: 'Show All',
+
+  // SETTINGS
+  SETTINGS: 'Settings',
+  TRANSLATION_NAME: 'English (US)',
+  CURRENCY: 'Currency',
+  LANGUAGE: 'Language',
+  THEME: 'Theme',
+  THEME_LIGHT: 'Light',
+  THEME_DARK: 'Dark',
 };
 
 export default en_US;
