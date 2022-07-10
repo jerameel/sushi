a8c7d31d0f2cbdaddf9d9cef29e7f4362c3b8dad
AronBrown <aronbrownofficialpage@gmail.com>
2022-07-10 14:50:42 +0200

    Added italian translation
    Inizial commit of the italian translation, not fully translated yet.

---
+++ src/constants/translations/it-IT.ts
@@ -0,0 +1,50 @@
+import { Translation } from 'types/Translation';
+
+const it_IT: Translation = {
+  // DASHBOARD
+  CREDIT: 'Credit',
+  DEBIT: 'Debit',
+  RECENT_TRANSACTIONS: 'Tranzazioni recenti',
+  MY_ACCOUNTS: 'My Accounts',
+
+  // ADD ACCOUNT
+  ADD_ACCOUNT: 'Add Account',
+  ACCOUNT_NAME: 'Account Name',
+  INITIAL_AMOUNT: 'Quantità Iniziale',
+
+  // ACCOUNT DETAILS
+  ACCOUNT_DETAILS: 'Account Details',
+  INITIAL_BALANCE: 'Saldo Iniziale',
+
+  // NEW TRANSACTION
+  NEW_TRANSACTION: 'Nuova Transazione',
+  CREATE_TRANSACTION: 'Crea Transizione',
+  CATEGORY: 'Categoria',
+  SOURCE_ACCOUNT: 'Account Di Origine',
+  DESTINATION_ACCOUNT: 'Account Di Destinazione',
+  TRANSFER: 'Trasferimento',
+  SHORT_DESCRIPTION: 'Breve Descrizione',
+  AMOUNT: 'Quantità',
+
+  // TRANSACTION DETAILS
+  ACCOUNT: 'Account',
+  DATE_CREATED: 'Data Di Creazione',
+  DATE_UPDATED: 'Data Di Aggiornamento',
+
+  // Transactions
+  TRANSACTIONS: 'Transazioni',
+  SEARCH: 'Cerca',
+  DATE_RANGE: 'Intervallo Date',
+  SHOW_ALL: 'Mostra Tutto',
+
+  // SETTINGS
+  SETTINGS: 'Impostazioni',
+  TRANSLATION_NAME: 'Italiano (IT)',
+  CURRENCY: 'Valuta',
+  LANGUAGE: 'Lingua',
+  THEME: 'Tema',
+  THEME_LIGHT: 'Chiaro',
+  THEME_DARK: 'Scuro',
+};
+
+export default it_IT;
