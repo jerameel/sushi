e9572fcd8baaff79cbfb70a931a2fe88c62021b6
Philosobrown <66909614+Philosobrown@users.noreply.github.com>
2022-07-18 02:25:26 +0200

    Added italian translation files (#24)
    Closes #11 
    
    Co-authored-by: AronBrown <aronbrownofficialpage@gmail.com>

--- src/constants/translations/index.ts
+++ src/constants/translations/index.ts
@@ -1,7 +1,9 @@
 import en_US from './en-US';
 import fil_PH from './fil-PH';
+import it_IT from './it-IT';
 
 export const TRANSLATIONS = {
   'en-US': en_US,
   'fil-PH': fil_PH,
+  'it-IT': it_IT,
 };
---
+++ src/constants/translations/it-IT.ts
@@ -0,0 +1,68 @@
+import { Translation } from 'types/Translation';
+
+const en_US: Translation = {
+  // DASHBOARD
+  CREDIT: 'Guadagno',
+  DEBIT: 'Spesa',
+  RECENT_TRANSACTIONS: 'Transazioni Recenti',
+  MY_ACCOUNTS: 'I Miei Account',
+
+  // ADD ACCOUNT
+  ADD_ACCOUNT: 'Aggiungi Account',
+  ACCOUNT_NAME: 'Nome Account',
+  INITIAL_AMOUNT: 'Quantità Iniziale',
+  CREATE_ACCOUNT: 'Crea Account',
+
+  // EDIT ACCOUNT
+  EDIT_ACCOUNT: 'Modifica Account',
+  UPDATE_ACCOUNT: 'Aggiorna Account',
+
+  // ACCOUNT DETAILS
+  ACCOUNT_DETAILS: 'Dettagli Account',
+  INITIAL_BALANCE: 'Bilancio Iniziale',
+  DELETE_ACCOUNT: 'Elimina Account?',
+  DELETE_ACCOUNT_INFO:
+    'Questo eliminerà definitivamente il portafoglio {{accountName}} comprese {{transactionCount}} transazioni collegate.',
+
+  // NEW TRANSACTION
+  NEW_TRANSACTION: 'Nuova Transazione',
+  CREATE_TRANSACTION: 'Crea Transazione',
+  CATEGORY: 'Categoria',
+  SOURCE_ACCOUNT: 'Account Di Origine',
+  DESTINATION_ACCOUNT: 'Account Di Destinazione',
+  TRANSFER: 'Trasferimento',
+  SHORT_DESCRIPTION: 'Breve Descrizione',
+  AMOUNT: 'Quantità',
+
+  // EDIT TRANSACTION
+  EDIT_TRANSACTION: 'Modifica Transazione',
+  UPDATE_TRANSACTION: 'Aggiorna Transazione',
+
+  // TRANSACTION DETAILS
+  TRANSACTION_DETAILS: 'Dettagli Transazione',
+  ACCOUNT: 'Account',
+  DATE_CREATED: 'Date Di Creazione',
+  DATE_UPDATED: 'Date Di Aggiornamento',
+  DELETE_TRANSACTION: 'Eliminare La Transazione?',
+  DELETE_TRANSACTION_INFO:
+    'Questo eliminerà definitivamente la transazione.',
+  KEEP: 'Mantieni',
+  DELETE: 'Elimina',
+
+  // Transactions
+  TRANSACTIONS: 'Transazioni',
+  SEARCH: 'Ricerca',
+  DATE_RANGE: 'Intervallo Di Date',
+  SHOW_ALL: 'Mostra Tutto',
+
+  // SETTINGS
+  SETTINGS: 'Impostazioni',
+  TRANSLATION_NAME: 'Italiano (IT)',
+  CURRENCY: 'Valuta',
+  LANGUAGE: 'Lingua',
+  THEME: 'Tema',
+  THEME_LIGHT: 'Chiaro',
+  THEME_DARK: 'Scuro',
+};
+
+export default it_IT;
