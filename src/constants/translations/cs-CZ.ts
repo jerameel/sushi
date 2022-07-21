2d8e9f4cae64c3748ba75a247384eae2b36c1971
Fjuro <fjuro@seznam.cz>
2022-07-21 06:10:51 +0200

    Add Czech language (#28)
    * Add Czech language translation entry
    
    * Create Czech translations for metadata

---
+++ src/constants/translations/cs-CZ.ts
@@ -0,0 +1,68 @@
+import { Translation } from 'types/Translation';
+
+const cs_CZ: Translation = {
+  // DASHBOARD
+  CREDIT: 'Kredit',
+  DEBIT: 'Debet',
+  RECENT_TRANSACTIONS: 'Nedávné transakce',
+  MY_ACCOUNTS: 'Mé účty',
+
+  // ADD ACCOUNT
+  ADD_ACCOUNT: 'Přidat účet',
+  ACCOUNT_NAME: 'Název účtu',
+  INITIAL_AMOUNT: 'Počáteční částka',
+  CREATE_ACCOUNT: 'Vytvořit účet',
+
+  // EDIT ACCOUNT
+  EDIT_ACCOUNT: 'Upravit účet',
+  UPDATE_ACCOUNT: 'Aktualizovat účet',
+
+  // ACCOUNT DETAILS
+  ACCOUNT_DETAILS: 'Podrobnosti účtu',
+  INITIAL_BALANCE: 'Počáteční zůstatek',
+  DELETE_ACCOUNT: 'Odstranit účet?',
+  DELETE_ACCOUNT_INFO:
+    'Tato akce nenávratně odstraní peněženku {{accountName}} včetně {{transactionCount}} připojených transakcí.',
+
+  // NEW TRANSACTION
+  NEW_TRANSACTION: 'Nová transakce',
+  CREATE_TRANSACTION: 'Vytvořit transakci',
+  CATEGORY: 'Kategorie',
+  SOURCE_ACCOUNT: 'Zdrojový účet',
+  DESTINATION_ACCOUNT: 'Cílový účet',
+  TRANSFER: 'Převést',
+  SHORT_DESCRIPTION: 'Krátký popis',
+  AMOUNT: 'Částka',
+
+  // EDIT TRANSACTION
+  EDIT_TRANSACTION: 'Upravit transakci',
+  UPDATE_TRANSACTION: 'Aktualizovat transakci',
+
+  // TRANSACTION DETAILS
+  TRANSACTION_DETAILS: 'Podrobnosti transakce',
+  ACCOUNT: 'Účet',
+  DATE_CREATED: 'Datum vytvoření',
+  DATE_UPDATED: 'Datum aktualizace',
+  DELETE_TRANSACTION: 'Odstranit transakci?',
+  DELETE_TRANSACTION_INFO:
+    'Tato akce nenávratně odstraní tento záznam o transakci.',
+  KEEP: 'Ponechat',
+  DELETE: 'Odstranit',
+
+  // Transactions
+  TRANSACTIONS: 'Transakce',
+  SEARCH: 'Hledat',
+  DATE_RANGE: 'Rozsah dat',
+  SHOW_ALL: 'Zobrazit vše',
+
+  // SETTINGS
+  SETTINGS: 'Nastavení',
+  TRANSLATION_NAME: 'Čeština',
+  CURRENCY: 'Měna',
+  LANGUAGE: 'Jazyk',
+  THEME: 'Téma',
+  THEME_LIGHT: 'Světlé',
+  THEME_DARK: 'Tmavé',
+};
+
+export default cs_CZ;
--- src/constants/translations/index.ts
+++ src/constants/translations/index.ts
@@ -1,8 +1,10 @@
+import cs_CZ from './cs-CZ';
 import en_US from './en-US';
 import fil_PH from './fil-PH';
 import it_IT from './it-IT';
 
 export const TRANSLATIONS = {
+  'cs-CZ': cs_CZ,
   'en-US': en_US,
   'fil-PH': fil_PH,
   'it-IT': it_IT,
