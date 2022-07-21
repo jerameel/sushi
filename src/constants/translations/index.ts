c3db754ed228d6968ee9f76c17f9b05e0184e8eb
SnakeOverflow <GitPublic.mo61w@aleeas.com>
2022-07-21 03:38:57 -0700

    Added Spanish Translation

--- src/constants/translations/index.ts
+++ src/constants/translations/index.ts
@@ -1,11 +1,13 @@
 import cs_CZ from './cs-CZ';
 import en_US from './en-US';
+import es_MX from './es-MX';
 import fil_PH from './fil-PH';
 import it_IT from './it-IT';
 
 export const TRANSLATIONS = {
   'cs-CZ': cs_CZ,
   'en-US': en_US,
+  'es-MX': es_MX,
   'fil-PH': fil_PH,
   'it-IT': it_IT,
 };
