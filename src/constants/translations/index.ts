4cc782f437681132848f7c3ad99b0f1c96e1c4fc
so1der <iluailua13@gmail.com>
2022-07-22 12:50:40 +0300

    Add Ukrainian language

--- src/constants/translations/index.ts
+++ src/constants/translations/index.ts
@@ -2,10 +2,12 @@ import cs_CZ from './cs-CZ';
 import en_US from './en-US';
 import fil_PH from './fil-PH';
 import it_IT from './it-IT';
+import uk_UA from './uk-UA';
 
 export const TRANSLATIONS = {
   'cs-CZ': cs_CZ,
   'en-US': en_US,
   'fil-PH': fil_PH,
   'it-IT': it_IT,
+  'uk-UA': uk_UA
 };
