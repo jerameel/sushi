527600707f7165b84e20e54fc747dbc99ee3bccf
so1der <103759422+so1der@users.noreply.github.com>
2022-07-28 16:57:43 +0300

    Add Ukrainian language (#33)

---
+++ src/constants/translations/uk-UA.ts
@@ -0,0 +1,68 @@
+import { Translation } from 'types/Translation';
+
+const uk_UA: Translation = {
+  // DASHBOARD
+  CREDIT: 'Кредит',
+  DEBIT: 'Дебет',
+  RECENT_TRANSACTIONS: 'Останні Транзакції',
+  MY_ACCOUNTS: 'Мої Рахунки',
+
+  // ADD ACCOUNT
+  ADD_ACCOUNT: 'Додати Рахунок',
+  ACCOUNT_NAME: 'Назва Рахунку',
+  INITIAL_AMOUNT: 'Початковий Баланс',
+  CREATE_ACCOUNT: 'Створити Рахунок',
+
+  // EDIT ACCOUNT
+  EDIT_ACCOUNT: 'Редагувати Рахунок',
+  UPDATE_ACCOUNT: 'Оновити Рахунок',
+
+  // ACCOUNT DETAILS
+  ACCOUNT_DETAILS: 'Деталі Рахунку',
+  INITIAL_BALANCE: 'Початковий Баланс',
+  DELETE_ACCOUNT: 'Видалити Рахунок?',
+  DELETE_ACCOUNT_INFO:
+    'Це назавжди видалить рахунок {{accountName}}, включаючи {{transactionCount}} пов’язаних транзакцій.',
+
+  // NEW TRANSACTION
+  NEW_TRANSACTION: 'Нова Транзакція',
+  CREATE_TRANSACTION: 'Створити Транзакцію',
+  CATEGORY: 'Категорія',
+  SOURCE_ACCOUNT: 'З Рахунку',
+  DESTINATION_ACCOUNT: 'На Рахунок',
+  TRANSFER: 'Переказ',
+  SHORT_DESCRIPTION: 'Короткий Опис',
+  AMOUNT: 'Сума',
+
+  // EDIT TRANSACTION
+  EDIT_TRANSACTION: 'Редагувати Транзакцію',
+  UPDATE_TRANSACTION: 'Оновити Транзакцію',
+
+  // TRANSACTION DETAILS
+  TRANSACTION_DETAILS: 'Деталі Транзакції',
+  ACCOUNT: 'Рахунок',
+  DATE_CREATED: 'Дата Створення',
+  DATE_UPDATED: 'Дата Оновлення',
+  DELETE_TRANSACTION: 'Видалити Транзакцію?',
+  DELETE_TRANSACTION_INFO:
+    'Це назавжди видалить запис транзацкії.',
+  KEEP: 'Лишити',
+  DELETE: 'Видалити',
+
+  // Transactions
+  TRANSACTIONS: 'Транзакції',
+  SEARCH: 'Пошук',
+  DATE_RANGE: 'Проміжок Часу',
+  SHOW_ALL: 'Показати Всі',
+
+  // SETTINGS
+  SETTINGS: 'Налаштування',
+  TRANSLATION_NAME: 'Українська (UA)',
+  CURRENCY: 'Валюта',
+  LANGUAGE: 'Мова',
+  THEME: 'Тема',
+  THEME_LIGHT: 'Світла',
+  THEME_DARK: 'Темна',
+};
+
+export default uk_UA;
