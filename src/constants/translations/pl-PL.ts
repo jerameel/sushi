1d95cad7e5bda221a33693e32e0d566cb1b06f4e
Maciej Klupp <100523238+maciej-klupp@users.noreply.github.com>
2022-07-29 10:44:01 +0200

    Added Polish translation

---
+++ src/constants/translations/pl-PL.ts
@@ -0,0 +1,68 @@
+import { Translation } from 'types/Translation';
+
+const pl_PL: Translation = {
+  // DASHBOARD
+  CREDIT: 'Wydatki',
+  DEBIT: 'Przychody',
+  RECENT_TRANSACTIONS: 'Ostatnia aktywność',
+  MY_ACCOUNTS: 'Konta',
+
+  // ADD ACCOUNT
+  ADD_ACCOUNT: 'Dodaj konto',
+  ACCOUNT_NAME: 'Nazwa konta',
+  INITIAL_AMOUNT: 'Początkowa kwota',
+  CREATE_ACCOUNT: 'Utwórz konto',
+
+  // EDIT ACCOUNT
+  EDIT_ACCOUNT: 'Edytuj konto',
+  UPDATE_ACCOUNT: 'Zaakceptuj zmiany',
+
+  // ACCOUNT DETAILS
+  ACCOUNT_DETAILS: 'Informacje o koncie',
+  INITIAL_BALANCE: 'Saldo początkowe',
+  DELETE_ACCOUNT: 'Usunąć konto?',
+  DELETE_ACCOUNT_INFO:
+    'Konto {{accountName}} zostanie bezpowrotnie usunięte wraz z {{transactionCount}} przypisanymi transakcjami.',
+
+  // NEW TRANSACTION
+  NEW_TRANSACTION: 'Nowa transakcja',
+  CREATE_TRANSACTION: 'Zaakceptuj transakcję',
+  CATEGORY: 'Kategoria',
+  SOURCE_ACCOUNT: 'Z konta',
+  DESTINATION_ACCOUNT: 'Na konto',
+  TRANSFER: 'Przelew',
+  SHORT_DESCRIPTION: 'Opis',
+  AMOUNT: 'Kwota',
+
+  // EDIT TRANSACTION
+  EDIT_TRANSACTION: 'Edytuj transakcję',
+  UPDATE_TRANSACTION: 'Zaakceptuj zmiany',
+
+  // TRANSACTION DETAILS
+  TRANSACTION_DETAILS: 'Informacje o transakcji',
+  ACCOUNT: 'Konto',
+  DATE_CREATED: 'Data utworzenia',
+  DATE_UPDATED: 'Data aktualizacji',
+  DELETE_TRANSACTION: 'Usunąć transakcję?',
+  DELETE_TRANSACTION_INFO:
+    'Wpis o transakcji zostanie bezpowrotnie usunięty.',
+  KEEP: 'Anuluj',
+  DELETE: 'Usuń',
+
+  // Transactions
+  TRANSACTIONS: 'Transakcje',
+  SEARCH: 'Wyszukaj',
+  DATE_RANGE: 'Okres',
+  SHOW_ALL: 'Pokaż wszystkie',
+
+  // SETTINGS
+  SETTINGS: 'Ustawienia',
+  TRANSLATION_NAME: 'Polski',
+  CURRENCY: 'Waluta',
+  LANGUAGE: 'Język',
+  THEME: 'Motyw',
+  THEME_LIGHT: 'Jasny',
+  THEME_DARK: 'Ciemny',
+};
+
+export default pl_PL;
