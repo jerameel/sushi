3bdcf7ffb4cc8fe0d41b819d110a035d624c14fe
Maciej Klupp <100523238+maciej-klupp@users.noreply.github.com>
2022-07-29 12:55:46 +0000

    Added Polish translation (#38)

--- src/constants/translations/index.ts
+++ src/constants/translations/index.ts
@@ -3,6 +3,7 @@ import en_US from './en-US';
 import es_MX from './es-MX';
 import fil_PH from './fil-PH';
 import it_IT from './it-IT';
+import pl_PL from './pl-PL';
 import ru_RU from './ru-RU';
 import uk_UA from './uk-UA';
 
@@ -12,6 +13,7 @@ export const TRANSLATIONS = {
   'es-MX': es_MX,
   'fil-PH': fil_PH,
   'it-IT': it_IT,
+  'pl-PL': pl_PL,
   'ru-RU': ru_RU,
   'uk-UA': uk_UA,
 };
