1ccf3e1fbe4758c594c960da348477a2eaf146d9
ayecptn <chrissir.01@web.de>
2022-09-01 21:16:35 +0200

    Added german translation

---
+++ src/constants/translations/de-DE.ts
@@ -0,0 +1,69 @@
+import { Translation } from 'types/Translation';
+
+const de_DE: Translation = {
+  // DASHBOARD
+  CREDIT: 'Kredit',
+  DEBIT: 'Debit',
+  RECENT_TRANSACTIONS: 'neuliche Transaktionen',
+  MY_ACCOUNTS: 'Meine Konten',
+
+  // ADD ACCOUNT
+  ADD_ACCOUNT: 'Konto hinzufügen',
+  ACCOUNT_NAME: 'Kontoname',
+  INITIAL_AMOUNT: 'Anfangsguthaben',
+  CREATE_ACCOUNT: 'Konto erstellen',
+
+  // EDIT ACCOUNT
+  EDIT_ACCOUNT: 'Konto editieren',
+  UPDATE_ACCOUNT: 'Konto updaten',
+
+  // ACCOUNT DETAILS
+  ACCOUNT_DETAILS: 'Kontodetails',
+  INITIAL_BALANCE: 'Startguthaben',
+  DELETE_ACCOUNT: 'Konto löschen?',
+  DELETE_ACCOUNT_INFO:
+    'Dies wird das {{accountName}} Konto mit {{transactionCount}} verknüpften Transaktionen permanent löschen.',
+
+  // NEW TRANSACTION
+  NEW_TRANSACTION: 'Neue Transaktion',
+  CREATE_TRANSACTION: 'Transaktion erstellen',
+  CATEGORY: 'Kategory',
+  SOURCE_ACCOUNT: 'Ursprungskonto',
+  DESTINATION_ACCOUNT: 'Zielkonto',
+  TRANSFER: 'Überweisung',
+  SHORT_DESCRIPTION: 'Kurze Beschreibung',
+  AMOUNT: 'Betrag',
+
+  // EDIT TRANSACTION
+  EDIT_TRANSACTION: 'Transaktion editieren',
+  UPDATE_TRANSACTION: 'Transaktion updaten',
+
+  // TRANSACTION DETAILS
+  TRANSACTION_DETAILS: 'Transaktiondetails',
+  ACCOUNT: 'Konto',
+  DATE_CREATED: 'Erstellungsdatum',
+  DATE_UPDATED: 'Aktualisierungsdatum',
+  DELETE_TRANSACTION: 'Transaktion löschen?',
+  DELETE_TRANSACTION_INFO:
+    'Dies wird den Transaktionseintrag permanent löschen.',
+  KEEP: 'Behalten',
+  DELETE: 'Löschen',
+
+  // Transactions
+  TRANSACTIONS: 'Transaktionen',
+  SEARCH: 'Suche',
+  DATE_RANGE: 'Datumsbereich',
+  SHOW_ALL: 'Alle anzeigen',
+  EXPORT: 'Exportieren',
+
+  // SETTINGS
+  SETTINGS: 'Einstellungen',
+  TRANSLATION_NAME: 'Deutsch (DE)',
+  CURRENCY: 'Währung',
+  LANGUAGE: 'Sprache',
+  THEME: 'Thema',
+  THEME_LIGHT: 'Hell',
+  THEME_DARK: 'Dunkel',
+};
+
+export default de_DE;
